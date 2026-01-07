5c1270471de6bd7e5b9967bec2972ddcd4c6eeec
devanshijodhani <devanshijodhani45@gmai.com>
2026-01-07 22:44:44 +0530

    add: user filter design

---
+++ src/pages/users/UsersFilter.tsx
@@ -0,0 +1,44 @@
+import { Button, Card, Col, Input, Row, Select } from 'antd';
+import { PlusOutlined } from '@ant-design/icons';
+
+const UsersFilter = () => {
+  return (
+    <Card>
+      <Row justify="space-between">
+        <Col span={16}>
+          <Row gutter={20}>
+            <Col span={12}>
+              <Input.Search placeholder="Search" />
+            </Col>
+            <Col span={6}>
+              <Select
+                placeholder="Select Role"
+                allowClear={true}
+                style={{ width: '100%' }}>
+                <Select.Option value="admin">Admin</Select.Option>
+                <Select.Option value="manager">Manager</Select.Option>
+                <Select.Option value="customer">Customer</Select.Option>
+              </Select>
+            </Col>
+            <Col span={6}>
+              <Select
+                placeholder="Status"
+                allowClear={true}
+                style={{ width: '100%' }}>
+                <Select.Option value="ban">Ban</Select.Option>
+                <Select.Option value="active">Active</Select.Option>
+              </Select>
+            </Col>
+          </Row>
+        </Col>
+        <Col span={8} style={{ display: 'flex', justifyContent: 'end' }}>
+          <Button type="primary" icon={<PlusOutlined />}>
+            Add User
+          </Button>
+        </Col>
+      </Row>
+    </Card>
+  );
+};
+
+export default UsersFilter;
