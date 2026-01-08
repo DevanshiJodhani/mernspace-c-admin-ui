093504d33242116bd588aed3d171594c96a843c6
devanshijodhani <devanshijodhani45@gmai.com>
2026-01-08 22:20:29 +0530

    add: tenants table design

--- src/pages/tenants/Tenants.tsx
+++ src/pages/tenants/Tenants.tsx
@@ -1,8 +1,42 @@
-import { Breadcrumb } from 'antd';
+import { Breadcrumb, Table } from 'antd';
 import { RightOutlined } from '@ant-design/icons';
 import { Link } from 'react-router-dom';
+import { useQuery } from '@tanstack/react-query';
+import type { Tenant } from '../../types';
+import { getTenants } from '../../http/api';
+
+const columns = [
+  {
+    title: 'ID',
+    dataIndex: 'id',
+    key: 'id',
+  },
+  {
+    title: 'Name',
+    dataIndex: 'name',
+    key: 'name',
+  },
+  {
+    title: 'Address',
+    dataIndex: 'address',
+    key: 'address',
+  },
+];
 
 const Tenants = () => {
+  const {
+    data: tenants,
+    isLoading,
+    isError,
+    error,
+  } = useQuery<Tenant[]>({
+    queryKey: ['tenants'],
+    queryFn: async () => {
+      const res = await getTenants();
+      return res.data.data;
+    },
+  });
+
   return (
     <>
       <Breadcrumb
@@ -12,6 +46,11 @@ const Tenants = () => {
           { title: 'Restaurants' },
         ]}
       />
+
+      {isLoading && <div>Loading...</div>}
+      {isError && <div>{error.message}</div>}
+
+      <Table columns={columns} dataSource={tenants} />
     </>
   );
 };
