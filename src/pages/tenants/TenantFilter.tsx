6e76c1f41a6570347b997399afe30cad3a75e0d4
devanshijodhani <devanshijodhani45@gmai.com>
2026-01-08 22:37:47 +0530

    add: tenant create button

--- src/pages/tenants/TenantFilter.tsx
+++ src/pages/tenants/TenantFilter.tsx
@@ -1,11 +1,12 @@
-import { Card, Col, Form, Input, Row } from 'antd';
+import { Button, Card, Col, Form, Input, Row } from 'antd';
+import { PlusOutlined } from '@ant-design/icons';
 
 const TenantFilter = () => {
   return (
     <Card>
       <Form>
         <Row>
-          <Col span={24}>
+          <Col span={16}>
             <Form.Item name="q">
               <Input.Search
                 placeholder="Search"
@@ -14,6 +15,11 @@ const TenantFilter = () => {
                 style={{ width: '100%' }}
               />
             </Form.Item>
+          </Col>
+          <Col span={8} style={{ display: 'flex', justifyContent: 'end' }}>
+            <Button type="primary" icon={<PlusOutlined />}>
+              Add Tenant
+            </Button>
           </Col>
         </Row>
       </Form>
